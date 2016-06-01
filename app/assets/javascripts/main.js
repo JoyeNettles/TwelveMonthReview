7b9bfb0cc632742868ea2b25503bd7ee27d9bb24
jnettles <jnettles@thoughtworks.com>
2016-06-01 00:32:58 -0400

    Header section done

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -34,3 +34,7 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
   }
 });
+
+$( window ).resize(function() {
+    $('.navbar').css('width', $(window).width());
+});
