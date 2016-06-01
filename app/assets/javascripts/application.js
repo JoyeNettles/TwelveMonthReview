54283c43a9a7a64b693f3f5372e2aec7f9ce416d
jnettles <jnettles@thoughtworks.com>
2016-05-31 23:37:18 -0400

    Worked on navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap.min
 //= require custom
+//= require main
 //= require jquery.hoverdir
 //= require jquery.hoverex.min
 //= require jquery.isotope.min
