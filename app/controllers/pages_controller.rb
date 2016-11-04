7eaab2e9b60d27d0df5f51dbf1adcbfc40c88159
jnettles <jnettles@thoughtworks.com>
2016-11-03 22:56:25 -0400

    Added new pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
 class PagesController < ApplicationController
+
+  def twelve
+
+  end
+
+  def eighteen
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#main'
+  get '/twelve' => 'pages#twelve'
+  get '/eighteen' => 'pages#eighteen'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
